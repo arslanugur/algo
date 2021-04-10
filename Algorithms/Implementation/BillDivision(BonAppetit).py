4f72c40b7ca1d859d586b968f53024ab42f1e6e5
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-11 01:55:58 +0300

    Create BillDivision(BonAppetit).py

---
+++ Algorithms/Implementation/BillDivision(BonAppetit).py
@@ -0,0 +1,24 @@
+https://www.hackerrank.com/challenges/bon-appetit/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+  
+#!/bin/python3
+
+import sys
+
+def bonAppetit(n, skip, b, ar):
+    sum_actual = 0
+    
+    ar.pop(skip)
+    sum_actual = sum(ar)//2
+    
+    if sum_actual == b:
+        return 'Bon Appetit'
+    else:
+        return b - sum_actual
+        
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+ar = list(map(int, input().strip().split(' ')))
+b = int(input().strip())
+result = bonAppetit(n, k, b, ar)
+print(result)
