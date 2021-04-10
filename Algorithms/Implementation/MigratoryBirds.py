c90639038a3a788c068397c5e64136fd3f967bf7
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-11 01:45:33 +0300

    Create MigratoryBirds.py

---
+++ Algorithms/Implementation/MigratoryBirds.py
@@ -0,0 +1,16 @@
+https://www.hackerrank.com/challenges/migratory-birds/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import sys
+
+def migratoryBirds(n, ar):
+    count = [0] * 6
+    
+    for bird in ar:
+        count[bird] += 1
+    
+    return count.index(max(count))
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = migratoryBirds(n, ar)
+print(result)
