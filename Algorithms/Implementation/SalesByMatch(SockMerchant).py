7772547e7893302120ad1ff9fc39ffce9077ae85
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-11 02:00:48 +0300

    Create SalesByMatch(SockMerchant).py

---
+++ Algorithms/Implementation/SalesByMatch(SockMerchant).py
@@ -0,0 +1,24 @@
+#https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#!/bin/python3
+
+import sys
+
+def sockMerchant(n, ar):
+    socks = {}
+    res = 0
+    
+    for el in ar:
+        if el not in socks.keys():
+            socks[el] = 1
+        else:
+            socks[el] += 1
+    
+    for key in socks.keys():
+        res += socks[key]//2
+    
+    return res
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = sockMerchant(n, ar)
+print(result)
