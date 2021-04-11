9b5d75e4d7cdded26b68738aad1d96d043432faf
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-12 01:21:46 +0300

    Update DayOfTheProgrammer.py

--- Algorithms/Implementation/DayOfTheProgrammer.py
+++ Algorithms/Implementation/DayOfTheProgrammer.py
@@ -1,4 +1,4 @@
-https://www.hackerrank.com/challenges/day-of-the-programmer/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#https://www.hackerrank.com/challenges/day-of-the-programmer/problem
 
 import sys
 
