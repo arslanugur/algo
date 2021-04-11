b037cf18888cc4fdccd915fefc4d755547cb2f15
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-12 01:04:44 +0300

    Create CountingValleys.py

---
+++ Algorithms/Implementation/CountingValleys.py
@@ -0,0 +1,28 @@
+#https://www.hackerrank.com/challenges/counting-valleys/problem
+
+import sys
+
+def countingValleys(n, s):
+    res = 0
+    in_valley = 0
+    curr = 0
+    
+    for step in s:
+        if step == 'U':
+            curr += 1
+        else:
+            curr -= 1
+        
+        if curr < 0 and in_valley == 0:
+            in_valley = 1
+        if in_valley == 1 and curr == 0:
+            in_valley = 0
+            res += 1
+
+    return res
+
+if __name__ == "__main__":
+    n = int(input().strip())     #The first line contains an integer steps, the number of steps in the hike.
+    s = input().strip()          #The second line contains a single string , path of steps characters that describe the path.
+    result = countingValleys(n, s)
+    print(result)
