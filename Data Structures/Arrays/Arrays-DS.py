5070caaddfea7e7c029b961d35fc21e13fd6344b
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-13 07:38:49 +0300

    Create Arrays-DS.py

---
+++ Data Structures/Arrays/Arrays-DS.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/arrays-ds/problem
+  
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+print(" ".join(map(str, arr[::-1])))
