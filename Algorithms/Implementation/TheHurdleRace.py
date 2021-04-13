575524c9e61ed6cfd1bd9f921ab60dc7cf30d491
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-13 07:23:26 +0300

    Create TheHurdleRace.py

---
+++ Algorithms/Implementation/TheHurdleRace.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/the-hurdle-race/problem
+
+#!/bin/python3
+
+import sys
+
+def hurdleRace(k, height):
+    return max(0, max(height) - k)
+
+if __name__ == "__main__":
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    height = list(map(int, input().strip().split(' ')))
+    result = hurdleRace(k, height)
+    print(result)
