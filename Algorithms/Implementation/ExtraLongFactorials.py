a989320aafc9c9dbfd22f8bb7176cd91d18873ed
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-22 00:10:44 +0300

    Create ExtraLongFactorials.py

---
+++ Algorithms/Implementation/ExtraLongFactorials.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+#!/bin/python3
+# this should work
+
+import sys
+from math import factorial
+
+def extraLongFactorials(n):
+    return factorial(n)
+    
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    print(extraLongFactorials(n))
