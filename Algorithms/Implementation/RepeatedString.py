fb1dda34f45aab211314fc52e2302c1ff0ac9912
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-22 00:28:38 +0300

    Create RepeatedString.py

---
+++ Algorithms/Implementation/RepeatedString.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/repeated-string/problem
+
+#!/bin/python3
+
+import sys
+
+def repeatedString(s, n):
+    return s.count('a') * (n//len(s)) + s[:n%len(s)].count('a')
+
+if __name__ == "__main__":
+    s = input().strip()
+    n = int(input().strip())
+    result = repeatedString(s, n)
+    print(result)
