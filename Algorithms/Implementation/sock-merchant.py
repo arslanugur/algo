d4cdf36318c2ab2e7b3da413f159acffda4c2d77
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-02 05:16:12 +0300

    Update and rename SalesByMatch(SockMerchant).py to sock-merchant.py

--- Algorithms/Implementation/sock-merchant.py
+++ Algorithms/Implementation/sock-merchant.py
@@ -1,4 +1,3 @@
-#https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 #!/bin/python3
 
 import sys
