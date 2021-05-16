281d99fbbffef885a1ef8a73e431f46db04f07b3
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-16 03:31:00 +0300

    Update print-the-elements-of-a-linked-list.py

--- Data Structures/Linked Lists/print-the-elements-of-a-linked-list.py
+++ Data Structures/Linked Lists/print-the-elements-of-a-linked-list.py
@@ -1,4 +1,15 @@
 #!/bin/python3
+"""
+ Print elements of a linked list on console
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+"""
 # Complete the printLinkedList function below.
 
 # For your reference:
