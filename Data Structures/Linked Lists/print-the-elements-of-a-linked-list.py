e6ae6944305367cc2a21e7babf995de50689a01f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-16 03:29:42 +0300

    Update print-the-elements-of-a-linked-list.py

--- Data Structures/Linked Lists/print-the-elements-of-a-linked-list.py
+++ Data Structures/Linked Lists/print-the-elements-of-a-linked-list.py
@@ -1,1 +1,25 @@
+#!/bin/python3
+# Complete the printLinkedList function below.
 
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+
+def printLinkedList(head):
+    node = head
+    while node != None:
+        print(node.data)
+        node = node.next
+
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
