610b1d8152184c4741000ed546db74964575f1b9
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-03 18:37:32 +0300

    Create string-construction.py

---
+++ Algorithms/Strings/string-construction.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import sys
+
+def stringConstruction(s):
+    return len(set(s))
+        
+
+if __name__ == "__main__":
+    q = int(input().strip())
+    for a0 in range(q):
+        s = input().strip()
+        result = stringConstruction(s)
+        print(result)
+        
+        
